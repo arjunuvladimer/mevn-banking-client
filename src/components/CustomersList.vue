1ff19a96a1e06071c5dd9747569f4db74277d391
arjunuvladimer <arjunuvladimer@gmail.com>
2022-06-17 10:31:00 +0530

    components with routing

---
+++ src/components/CustomersList.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1> Customer List Component</h1>
+    </div>
+</template>
+<script>
+    export default{
+        name:'CustomerList'
+    }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../components/Home.vue')
+    },
+    {
+        path:'/addCustomer',
+        name: 'AddCustomer',
+        component: () => import('../components/AddCustomer.vue')
+    },
+    {
+        path:'/customers',
+        name: 'CustomerList',
+        component: () => import('../components/CustomersList.vue')
+    },
+
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
